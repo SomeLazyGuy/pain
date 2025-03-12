cfe8e3e2fe905cf22cd2310795276bded029b3c3
PaulConnorR <paul-rasche@web.de>
2025-03-12 12:37:27 +0100

    Update JumpState.cs

--- Assets/Scripts/PlayerStates/JumpState.cs
+++ Assets/Scripts/PlayerStates/JumpState.cs
@@ -3,22 +3,21 @@ using UnityEngine;
 public class JumpState : State
 {
     private PlayerStateMachine _stateMachine;
-    public float jumpVelocity;
     
     override public void Entry(PlayerStateMachine stateMachine)
     {
         Debug.Log("Jump Entry");
         _stateMachine = stateMachine;
-        jumpVelocity = _stateMachine.jumpForce;
+        _stateMachine.JumpVelocity = _stateMachine.jumpForce;
     }
     override public void Update()
     {
         _stateMachine.Rb.linearVelocity = new Vector2(_stateMachine.MoveDirection.x 
-                                                       * _stateMachine.moveSpeed, jumpVelocity);
-        jumpVelocity -= _stateMachine.jumpDeceleration;
-        if (jumpVelocity < 0)
+                                                       * _stateMachine.moveSpeed, _stateMachine.JumpVelocity);
+        _stateMachine.JumpVelocity -= _stateMachine.jumpDeceleration;
+        if (_stateMachine.JumpVelocity < 0)
         {
-            jumpVelocity = 0;
+            _stateMachine.JumpVelocity = 0;
         }
     }
     override public void Exit()
