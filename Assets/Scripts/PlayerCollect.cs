f94417fb28da5de2d07c33487b3fd70c8d0de7bb
SomeLazyGuy <85708571+SomeLazyGuy@users.noreply.github.com>
2025-03-13 12:41:19 +0100

    Add key

--- Assets/Scripts/PlayerCollect.cs
+++ Assets/Scripts/PlayerCollect.cs
@@ -1,16 +1,28 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerCollect : MonoBehaviour {
     [SerializeField] private Points points;
+
+    public UnityEvent keyPickedUpEvent;
+
+    private void Start() {
+        if (keyPickedUpEvent == null) {
+            keyPickedUpEvent = new UnityEvent();
+        }
+    }
     
     private void OnTriggerStay2D(Collider2D other) {
         if (other.CompareTag("Collectable")) {
             int value = other.GetComponent<Leaf>().value;
             Destroy(other.gameObject);
-            Debug.Log(value);
-            
             points.AddPoints(value);
+        }
+        
+        if (other.CompareTag("Key")) {
+            Destroy(other.gameObject);
+            keyPickedUpEvent.Invoke();
         }
     }
 }
