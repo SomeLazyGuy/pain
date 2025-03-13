3c3be968aed73d09d568174d6a6c6ec8477477df
PaulConnorR <paul-rasche@web.de>
2025-03-13 12:49:51 +0100

    doooooor

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public void OpenDoor()
+    {
+        Destroy(transform.GetChild(0));
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+    }
+}
